d83fdf004c004ac3ee2df56759ded478f794d5ac
Peerapat Toprasert <peerapat.toprasert@gmail.com>
2023-04-19 00:31:02 +0700

    first commit

---
+++ scripts/delete-conflict-dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -Rf ../../node_modules/.pnpm/apollo-server-types@3* \
+    ../../node_modules/.pnpm/apollo-server-plugin-base@3* \
+    ../../node_modules/.pnpm/apollo-server-core@3*
