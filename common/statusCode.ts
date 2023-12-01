7930280172f19e2fd39e5b876014ee11bf0b789b
FirstRe <firstzaxshot95@gmail.com>
2023-12-01 21:07:04 +0700

    feat: add statusCode

--- common/statusCode.ts
+++ common/statusCode.ts
@@ -1,23 +1,51 @@
+interface ApiResponse<T> {
+  data: T
+  status: {
+    code: number
+    message: string
+  }
+}
+
 export const statusCode = {
-  success: {
-    code: 1000,
-    message: 'success'
+  success: <T>(data: T): ApiResponse<T> => {
+    return { data, status: { code: 1000, message: 'success' } }
   },
   forbidden: {
-    code: 9001,
-    message: 'FORBIDDEN'
+    //wrong user
+    data: null,
+    status: {
+      code: 9001,
+      message: 'FORBIDDEN'
+    }
   },
   duplicated: {
-    code: 9002,
-    message: 'Duplicated Name'
+    data: null,
+    status: {
+      code: 9002,
+      message: 'Duplicated Name'
+    }
   },
   badRequest: {
-    code: 9003,
-    message: 'Bad Request'
+    data: null,
+    status: {
+      code: 9003,
+      message: 'Bad Request'
+    }
+  },
+  onUse: {
+    //badhavetype on use
+    data: null,
+    status: {
+      code: 9004,
+      message: 'On Use'
+    }
   },
   internalError: {
-    code: 9999,
-    message: 'Internal Error'
+    data: null,
+    status: {
+      code: 9999,
+      message: 'Internal Error'
+    }
   }
 }
 
